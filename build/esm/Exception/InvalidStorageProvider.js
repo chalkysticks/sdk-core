b2ef7af55922f0957fea6733dc5fe2f3db1d0675
Matt Kenefick <keneficksays@gmail.com>
2022-12-24 12:37:47 -0500

    .

--- build/esm/Exception/InvalidStorageProvider.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class InvalidStorageProvider extends Error {
-}
-exports.default = InvalidStorageProvider;
-//# sourceMappingURL=InvalidStorageProvider.js.map
--- build/esm/Exception/Unauthorized.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Unauthorized extends Error {
-}
-exports.default = Unauthorized;
-//# sourceMappingURL=Unauthorized.js.map
