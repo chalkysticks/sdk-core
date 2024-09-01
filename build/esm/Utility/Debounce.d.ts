9866235b6a74bb1c278c0612e1cfe9fe17f19db5
Matt Kenefick <keneficksays@gmail.com>
2024-09-01 13:26:20 -0400

    .

--- build/esm/Utility/Debounce.d.ts
+++ build/esm/Utility/Debounce.d.ts
@@ -1,6 +1,5 @@
 export declare class Debounce {
     static exec(reference: symbol | string, callable: () => void, timeout?: number, inclusive?: boolean, args?: any[]): void;
-    private static instances;
     callback: () => void;
     threshold: number;
     private inclusive;
