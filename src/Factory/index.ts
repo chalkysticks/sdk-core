b9febd2d3a8c3e576543a4cfc5d7e5ed76b9803d
Matt Kenefick <keneficksays@gmail.com>
2025-05-02 08:54:32 -0400

    Remove fact, rulebook, and statistic. Moved to extras.

--- src/Factory/index.ts
+++ src/Factory/index.ts
@@ -1,9 +1,6 @@
 export * as Advertisement from './Advertisement.js';
 export * as Content from './Content.js';
-export * as Fact from './Fact.js';
 export * as Feed from './Feed.js';
 export * as Geocode from './Geocode.js';
-export * as Rulebook from './Rulebook.js';
-export * as Statistic from './Statistic.js';
 export * as User from './User.js';
 export * as Wallet from './Wallet.js';
